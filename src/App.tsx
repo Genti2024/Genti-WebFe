214d1a2b2d6f8e424098479a638405712c49f6e6
leegurts <rmstjd333@gmail.com>
2024-04-09 17:53:13 +0900

    [INIT] 프로젝트 초기 설정

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <div className="font-bold text-lg bg-slate-300">Genti</div>
+            <div className="text-3xl font-bold underline">Hello world!</div>
+        </>
+    );
+}
+
+export default App;
